22f09cc7b5709cf81c64ef50af530c8d97202704
Khaleeb <cdavis1530@gmail.com>
2018-01-26 09:51:30 -0500

    Final

--- triad.py
+++ triad.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # A script to show all the major, minor, and diminished triads on a fretboard
+import sys
 
 
 # Fretboard Point Matrix
@@ -23,4 +24,66 @@ def pboard():
         print("")
 
 
+Notes = ["A","A#","B","C","C#","D","D#","E","F","F#","G","G#"]
+
+
+def Calc(root,ty):
+    global first
+    global third
+    global fifth
+    if(ty == 'M'):
+        three = 4
+        five = 7
+    elif(ty == 'm'):
+        three = 3
+        five = 7
+    elif(ty == 'd' or ty == 'D'):
+        three = 3
+        five = 6
+    else:
+        print("Input Error")
+        sys.exit()
+
+
+    first = root
+    third = root + three
+    fifth = root + five
+    if(third > 11):
+        third = third - 12
+    if(fifth > 11):
+        fifth = fifth - 12
+
+
+def Indexer(a,b,c):
+    for s in range(6):
+        for f in range(25):
+            index = (s * 25) + f
+            if(s == 0):
+                I = (f + 7) % 12
+            elif(s == 1):
+                I = (f + 2) % 12
+            elif(s == 2):
+                I = (f + 10) % 12
+            elif(s == 3):
+                I = (f + 5) % 12
+            elif(s == 4):
+                I = (f % 12)
+            elif(s == 5):
+                I = (f + 7) %  12
+
+            if(I == a):
+                Points[s][f] = "1"
+            elif(I == b):
+                Points[s][f] = "3"
+            elif(I == c):
+                Points[s][f] = "5"
+
+
+inp = input("Root Note(# only): ")
+root = Notes.index(inp)
+ty = input("M/m/D: ")
+Calc(root,ty)
+Indexer(first,third,fifth)
+print ("1: %s\n3: %s\n5: %s" % (Notes[first],Notes[third],Notes[fifth]))
+print("0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4")
 pboard()
